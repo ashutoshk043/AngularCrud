5987ab916583b1b05f9e8130eb9adc82846f7aef
vsehrawat <vishant.itm@outlook.com>
2023-01-20 17:00:35 +0530

    underProgress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud';
+  registerForm!: FormGroup;
+
+  constructor(private _tostr: ToastrService, private _fb: FormBuilder) {
+
+  }
+
+  ngOnInit() {
+    this.setFormState();
+  }
+
+  setFormState() {
+    this.registerForm = this._fb.group({
+      id: [0],
+      title: ['', Validators.required],
+      firstName: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
+      lastName: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
+      email: ['', Validators.compose([Validators.email, Validators.required])],
+      dob: ['', Validators.compose([Validators.required, Validators.pattern(/^([0-2^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/)])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      confirmPassword: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      acceptTerms: [false, Validators.required]
+
+    })
+  }
+  onSubmit(){
+      
+  }
+
+  onCancel(){
+    this.registerForm.reset();
+  }
 }
